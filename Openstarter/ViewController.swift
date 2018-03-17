dc6a5221307ed53c14a1d0cc37e108d7b62cf51f
Mohamed Kalia <mohamedkalia@mohameds-macbook-pro.local>
2018-03-17 21:52:13 +0100

    Initial Commit

---
+++ Openstarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Openstarter
+//
+//  Created by Mohamed Kalia on 3/17/18.
+//  Copyright © 2018 Mohamed Kalia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
