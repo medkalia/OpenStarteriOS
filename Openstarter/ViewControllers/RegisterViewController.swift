c6c1beae39baa2f036ccbfa238f221a9b9ee9c0e
mohamedkalia <mohamed.kalia@esprit.tn>
2018-03-29 22:10:16 +0100

    fixes

--- Openstarter/ViewControllers/RegisterViewController.swift
+++ Openstarter/ViewControllers/RegisterViewController.swift
@@ -21,7 +21,10 @@ class RegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func exit(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
