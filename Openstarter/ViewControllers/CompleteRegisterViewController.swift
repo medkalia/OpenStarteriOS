c6be10ec7770bc21b691a33c5e627f1937f317c9
mohamedkalia <mohamed.kalia@esprit.tn>
2018-03-30 00:34:54 +0100

    clean up

---
+++ Openstarter/ViewControllers/CompleteRegisterViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CompleteRegisterViewController.swift
+//  Openstarter
+//
+//  Created by Mohamed Kalia on 3/29/18.
+//  Copyright © 2018 Mohamed Kalia. All rights reserved.
+//
+
+import UIKit
+
+class CompleteRegisterViewController: UIViewController {
+    
+    var colors: [UIColor] = [UIColor(hue: 0.5444, saturation: 0.8, brightness: 0.54, alpha: 1.0), UIColor(hue: 0.5667, saturation: 0.99, brightness: 0.72, alpha: 1.0),UIColor(hue: 0.5833, saturation: 0.25, brightness: 0.27, alpha: 1.0), UIColor(hue: 0.8583, saturation: 0.16, brightness: 0.5, alpha: 1.0)]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.startAnimation(index: 0)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startAnimation(index: Int) {
+        UIView.animate(withDuration: 2.5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: {
+            self.view.backgroundColor = self.colors[index]
+        }) { (finished) in
+            var currentIndex = index + 1
+            if currentIndex == self.colors.count { currentIndex = 0 }
+            self.startAnimation(index: currentIndex)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
