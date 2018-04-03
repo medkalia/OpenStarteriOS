051e077bccb2ee30c9fdd2b27e52c91c9c686889
tarek <marwenvic99@gmail.com>
2018-04-03 01:21:46 +0100

    add project

---
+++ Openstarter/ViewControllers/AddProjectViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddProjectViewController.swift
+//  Openstarter
+//
+//  Created by tarek on 03/04/2018.
+//  Copyright © 2018 Mohamed Kalia. All rights reserved.
+//
+
+import UIKit
+
+class AddProjectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "background"))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        //UIImage *img = [UIImage imageNamed:@"star"];
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
