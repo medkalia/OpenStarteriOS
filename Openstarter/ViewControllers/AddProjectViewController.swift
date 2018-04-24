615b8ff236c072e4b2165f769456d65cfd24cf0d
mohamedkalia <mohamed.kalia@esprit.tn>
2018-04-24 03:59:52 +0100

    add working final

--- Openstarter/ViewControllers/AddProjectViewController.swift
+++ Openstarter/ViewControllers/AddProjectViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SwiftyPickerPopover
 
 
 class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPickerViewDelegate {
     
+    var json : JSON = []
+    var groupId : String = ""
     @IBOutlet weak var nameProject: LoginTextField!
     
     @IBOutlet weak var budhget: UITextField!
@@ -21,24 +24,19 @@ class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPicke
     @IBOutlet weak var shortDesc: LoginTextField!
     @IBOutlet weak var startDate: UITextField!
     @IBOutlet weak var finishDate: UITextField!
-    
-    
+    @IBOutlet weak var selectedGroup: UITextField!
     let datePicker = UIDatePicker()
-    
-    @IBOutlet weak var pickerGroups: UIPickerView!
-    
     @IBOutlet weak var pickerCategorie: UIPickerView!
     var dataCategorie: [String] = [String]()
-    var dataGroup = [String]()
-    let cs = ConnectionToServer()
-    var groupArray: [[String : AnyObject]] = [[String : AnyObject]]()
     
+    let cs = ConnectionToServer()
+   
+     let email = UserDefaults.standard.string(forKey: "userEmail")
     var category : String = ""
     
     
     override func viewDidLoad() {
-        pickerGroups.dataSource=self
-        pickerGroups.delegate=self
+        
         pickerCategorie.dataSource=self
         pickerCategorie.delegate=self
         super.viewDidLoad()
@@ -50,26 +48,73 @@ class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPicke
         
         createDatePicker()
         createDatePickerTwo()
-        print(self.groupArray.count)
+       
+        
+    }
+    
+    @IBAction func didTapStringPickerWithTextField(_ sender: UITextField) {
+        var names : [String] = []
+        for groupName in json.arrayValue {
+            names.append(groupName["name"].string!)
+            print(groupName["id"])
+            print(groupName["name"])
+        }
+        
+        StringPickerPopover(title: "Groups", choices: names)
+            .setDoneButton(action: { popover, selectedRow, selectedString in
+                //sender.text = selectedString
+                self.selectedGroup.text = selectedString
+                
+                for groupItem in self.json.arrayValue {
+                    if groupItem["name"].string == selectedString {
+                       // self.groupId = groupItem["id"].string!
+                       
+                        self.groupId = groupItem["id"].stringValue
+                        print("intern",self.groupId)
+                    }
+                    
+                }
+                
+                //self.groupId = selectedString
+            })
+            .appear(originView: sender, baseViewController: self)
         
     }
     
     @IBAction func addProject(_ sender: Any) {
         
         let parameters: [String: Any] = [
-            "email" :  "mohamed.kalia@esprit.tn",
+           
             "name" : nameProject.text! as String,
-            "startDate" : startDate.text! as String,
-            "finishDate" : finishDate.text! as String,
+            "startDate" : "2018/01/13 08:59:06",
+            "finishDate" : "2019/01/13 08:59:06",
             "description" : desc.text! as String,
             "shortDescription" : shortDesc.text! as String,
             "budget" : budhget.text! as String,
-            "category" : category,
-            "collaborationGroup" : ""
+            "id_category" : "1",
+            "id_group" : self.groupId,
+            "servicesList" : "",
+            "equipementsList" : ""
             
         ]
         
         print(parameters)
+        
+        Alamofire.request(cs.url+"/project/create", method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                self.json = JSON(value)
+                
+            case .failure(let error):
+                print(error)
+                let message = "cannot reach server "
+                let alert2 = UIAlertController(title: "error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+                alert2.addAction(UIAlertAction(title: "Okey", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert2, animated: true, completion: nil)
+            }
+        }
+        
+        
     }
     func createDatePicker(){
         let toolbar = UIToolbar()
@@ -101,30 +146,23 @@ class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPicke
     
     
     func fetchGroups(){
-        let parameters: [String: Any] = [
-            "email" :  "mohamed.kalia@esprit.tn"
+        let getParameters: [String: Any] = [
+            "email" :  self.email!
             
         ]
-        Alamofire.request(cs.url+"/collaborationGroup/getByAdminUser", method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseJSON(completionHandler:{response in
-            switch response.result{
-            case .success:
-                print("validation succesful")
-                print(response.result.value!)
-                self.groupArray = (response.result.value! as! [[String : AnyObject]] )
-                print(self.groupArray.count)
-                print(self.groupArray[0]["name"]!)
-                print("heeyyyyy")
-                for station in self.groupArray{
-                    self.dataGroup = [station["name"]  as! String]
-                }
-                print(self.dataGroup,self.dataGroup.count)
+        Alamofire.request(cs.url+"/collaborationGroup/getByUser", method: .post, parameters: getParameters, encoding: JSONEncoding.default).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                self.json = JSON(value)
                 
             case .failure(let error):
-                print("leeeeeee")
                 print(error)
+                let message = "cannot reach server "
+                let alert2 = UIAlertController(title: "error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+                alert2.addAction(UIAlertAction(title: "Okey", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert2, animated: true, completion: nil)
             }
-            
-        } )
+        }
     }
     
     
@@ -133,22 +171,13 @@ class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPicke
     }
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
-        var nbr : Int = dataGroup.count
-        print( "aaa",groupArray.count)
-        if(pickerView == pickerCategorie){
-            nbr = dataCategorie.count
+      return dataCategorie.count
         }
         
-        return nbr
-    }
+    
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if(pickerView == pickerGroups){
-            
-            let titleRow = dataGroup[row]
-            print("mamamamamamamamamaaaa"+titleRow)
-            
-        }
-        else if (pickerView == pickerCategorie){
+     if (pickerView == pickerCategorie){
             
             let titleRow = dataCategorie[row]
             return titleRow
@@ -157,10 +186,7 @@ class AddProjectViewController: UIViewController ,UIPickerViewDataSource,UIPicke
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        if(pickerView == pickerGroups){
-            //print(dataGroup[row])
-        }
-        else if (pickerView == pickerCategorie){
+        if (pickerView == pickerCategorie){
             print(dataCategorie[row])
             category = dataCategorie[row]
         }
